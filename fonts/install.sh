29c45a6473b442134e970db31863d4155b869d68
Louis Kounios <louis@cergos.io>
2018-11-15 14:37:23 +0200

    Overwrite fonts and clean up /tmp/

--- fonts/install.sh
+++ fonts/install.sh
@@ -4,11 +4,12 @@ URL="https://github.com/tonsky/FiraCode/releases/download/1.206/FiraCode_1.206.z
 
 install() {
 	curl -L -s -o /tmp/fira.zip "$URL"
-	unzip /tmp/fira.zip -d /tmp/FiraCode
+	unzip /tmp/fira.zip -o -d /tmp/FiraCode
 	# Do not install the 'Bold' variant as it shares a numerical weight (600)
 	# with the 'Retina' variant, interfering with some configurations.
 	mkdir -p "$1" && \
-		cp /tmp/FiraCode/otf/{FiraCode-Light.otf,FiraCode-Medium.otf,FiraCode-Regular.otf,FiraCode-Retina.otf} "$1"
+		cp -f /tmp/FiraCode/otf/{FiraCode-Light.otf,FiraCode-Medium.otf,FiraCode-Regular.otf,FiraCode-Retina.otf} "$1"
+	rm -rf /tmp/fira.zip /tmp/FiraCode
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then

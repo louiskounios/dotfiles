e3a30aafc4c0c0d4590af2c95bfb131fdadbdb1e
Louis Kounios <louis@cergos.io>
2018-11-18 11:43:37 +0200

    Fix issue with font installation script

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,13 +3,13 @@
 URL="https://github.com/tonsky/FiraCode/releases/download/1.206/FiraCode_1.206.zip"
 
 install() {
+	rm -rf /tmp/fira.zip /tmp/FiraCode
 	curl -L -s -o /tmp/fira.zip "$URL"
-	unzip /tmp/fira.zip -o -d /tmp/FiraCode
+	unzip /tmp/fira.zip -d /tmp/FiraCode
 	# Do not install the 'Bold' variant as it shares a numerical weight (600)
 	# with the 'Retina' variant, interfering with some configurations.
 	mkdir -p "$1" && \
-		cp -f /tmp/FiraCode/otf/{FiraCode-Light.otf,FiraCode-Medium.otf,FiraCode-Regular.otf,FiraCode-Retina.otf} "$1"
-	rm -rf /tmp/fira.zip /tmp/FiraCode
+		cp /tmp/FiraCode/otf/{FiraCode-Light.otf,FiraCode-Medium.otf,FiraCode-Regular.otf,FiraCode-Retina.otf} "$1"
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then

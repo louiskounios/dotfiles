c5d80e6a8938cfb029f0e9d69edc1f575f4ac18b
Louis Kounios <louis@cergos.io>
2018-12-16 19:51:24 +0200

    Install prettier.io extension

--- vscode/install.sh
+++ vscode/install.sh
@@ -17,6 +17,7 @@ if test "$(which code)"; then
 casualjim.gotemplate
 deerawan.vscode-dash
 EditorConfig.EditorConfig
+esbenp.prettier-vscode
 mgmcdermott.vscode-language-babel
 ms-python.python
 ms-vscode.Go

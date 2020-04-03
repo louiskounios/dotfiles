205d1b3ab8225dd6fb8bbbd85e770e1db3338b97
Louis Kounios <louis@cergos.io>
2020-04-03 12:00:36 +0300

    Use `bat` as `man` pager

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,6 +16,8 @@ export EXA_COLORS='fi=38;5;015:di=38;5;038:ex=38;5;048:ur=38;5;015:uw=38;5;203:u
 
 export BAT_THEME=OneHalfDark
 export BAT_STYLE="grid,header"
+# Use bat as `man` pager.
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 # Make Ctrl+W useful by deleting until a slash or period is encountered.
 export WORDCHARS='*?_-[]~=&;!#$%^(){}<>'

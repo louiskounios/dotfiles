1ba0bf12ad7ea904d0fc53da408ce79bf5f45285
Louis Kounios <louis@cergos.io>
2021-04-06 13:47:35 +0300

    Update exa version

--- exa/install.sh
+++ exa/install.sh
@@ -3,7 +3,7 @@
 if [ "$(uname -s)" = "Darwin" ]; then
 	brew install exa
 else
-	wget "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip" --output-document=/tmp/exa.zip
+	wget "https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-0.10.0.zip" --output-document=/tmp/exa.zip
 	unzip /tmp/exa.zip -d /tmp
 	rm --force /tmp/exa.zip
 	sudo mv /tmp/exa-linux-x86_64 /usr/local/bin/exa

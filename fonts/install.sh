7126a238e068b0935cd303d7dcb73ef4725bb87f
Louis Kounios <louis@cergos.io>
2021-04-06 14:45:55 +0300

    Update font-installation logic

--- fonts/install.sh
+++ fonts/install.sh
@@ -6,13 +6,9 @@ install() {
 	rm -rf /tmp/fira.zip /tmp/FiraCode "$1"/FiraCode*.otf
 	curl -L -s -o /tmp/fira.zip "$URL"
 	unzip /tmp/fira.zip -d /tmp/FiraCode
-	# Do not install the 'Bold' variant as it shares a numerical weight (600)
-	# with the 'Retina' variant, interfering with some configurations.
 	mkdir -p "$1" && \
-		cp /tmp/FiraCode/otf/FiraCode-Light.otf "$1" && \
-		cp /tmp/FiraCode/otf/FiraCode-Medium.otf "$1" && \
-		cp /tmp/FiraCode/otf/FiraCode-Regular.otf "$1" && \
-		cp /tmp/FiraCode/otf/FiraCode-Retina.otf "$1"
+		cp /tmp/FiraCode/variable_ttf/FiraCode-VF.ttf "$1" && \
+		cp /tmp/FiraCode/ttf/*.ttf "$1"
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then

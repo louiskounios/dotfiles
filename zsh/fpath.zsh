87674b5aa89cef8765982744ac0cf054895f3ccb
Louis Kounios <louis@cergos.io>
2021-04-06 19:33:15 +0300

    Add zsh completions on macOS+arm64 to fpath

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+if [ "$(uname -s)" = "Darwin" ]; then
+	if [ "$(arch)" = "arm64" ]; then
+		fpath=('/opt/homebrew/share/zsh/site-functions' '/opt/homebrew/share/zsh/functions' $fpath)
+	fi
+fi
+
 # Add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder in $DOTFILES/*; do
 	if [ -d "$topic_folder" ]; then

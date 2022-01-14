dfb4b29cfc8e209fd5595b56a3d49d52c97e727c
Louis Kounios <louis@cergos.io>
2022-01-14 18:15:54 +0200

    Update bat version

--- bat/install.sh
+++ bat/install.sh
@@ -3,7 +3,7 @@
 if [ "$(uname -s)" = "Darwin" ]; then
 	brew install bat
 else
-	wget "https://github.com/sharkdp/bat/releases/download/v0.18.0/bat_0.18.0_amd64.deb" --output-document=/tmp/bat.deb
+	wget "https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb" --output-document=/tmp/bat.deb
 	sudo dpkg --install /tmp/bat.deb
 	rm --force /tmp/bat.deb
 fi

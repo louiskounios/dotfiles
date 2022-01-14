09e2036d4ce69a4e066fbffe7bfb6d3b2819df49
Louis Kounios <louis@cergos.io>
2022-01-14 18:57:52 +0200

    Fix exa installation

--- exa/install.sh
+++ exa/install.sh
@@ -6,5 +6,6 @@ else
 	wget "https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip" --output-document=/tmp/exa.zip
 	unzip /tmp/exa.zip -d /tmp
 	rm --force /tmp/exa.zip
-	sudo mv /tmp/exa-linux-x86_64 /usr/local/bin/exa
+	sudo mv /tmp/bin/exa /usr/local/bin/
+	sudo mv /tmp/completions/exa.zsh /usr/local/share/zsh/site-functions/
 fi

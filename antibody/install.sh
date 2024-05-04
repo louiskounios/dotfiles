149ddd2e573fa8eb8cf2316ee2578facba5da8c4
Louis Kounios <louis@cergos.io>
2024-05-04 09:24:18 +0300

    Fix antibody brew install

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if which brew >/dev/null 2>&1; then
-	brew install getantibody/tap/antibody || brew upgrade antibody
+	brew install antibody || brew upgrade antibody
 else
 	curl -sL https://git.io/antibody | sudo sh -s - -b /usr/local/bin
 fi

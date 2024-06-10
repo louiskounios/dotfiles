f74d8cc523dc9267a6df2efe462b40c6f83d6aed
Louis Kounios <louis@cergos.io>
2024-06-10 17:02:46 +0300

    Install and use antidote on Linux

--- antidote/install.sh
+++ antidote/install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-brew install antidote || brew upgrade antidote
-
-# source "$(brew --prefix)/opt/antidote/share/antidote/antidote.zsh"
-# source <(antidote init)
-# antidote bundle < "$DOTFILES/antidote/bundles.txt" > "$HOME/.zsh_plugins.txt"
-# antidote update
+if [ "$(uname -s)" = "Darwin" ]; then
+	brew install antidote || brew upgrade antidote
+else
+	ANTIDOTE_REPO_DIR="${HOME}/.antidote"
+	if [ ! -d "$ANTIDOTE_REPO_DIR" ]; then
+		git clone --depth=1 https://github.com/mattmc3/antidote.git "$ANTIDOTE_REPO_DIR"
+	else
+		cd "$ANTIDOTE_REPO_DIR"
+		git pull
+	fi
+fi
